4dba0dec9423e8d860b2b650212db2e1c30086cd
chinyanga <chinyangaf@gmail.com>
2021-01-15 07:43:58 +0200

    added model and input validator

--- src/main/java/com/playsafe/conversions/models/UnitConversions.java
+++ src/main/java/com/playsafe/conversions/models/UnitConversions.java
@@ -1,5 +1,48 @@
 package com.playsafe.conversions.models;
 
 public class UnitConversions {
+	
+	private String requestStatus="Success";
+	private String inputType;
+	private double input;
+	private String outputType;
+	private double result;
+	public UnitConversions(String requestStatus, String inputType, double input, String outputType, double result) {
+		this.requestStatus = requestStatus;
+		this.inputType = inputType;
+		this.input = input;
+		this.outputType = outputType;
+		this.result = result;
+	}
+	public String getRequestStatus() {
+		return requestStatus;
+	}
+	public void setRequestStatus(String requestStatus) {
+		this.requestStatus = requestStatus;
+	}
+	public String getInputType() {
+		return inputType;
+	}
+	public void setInputType(String inputType) {
+		this.inputType = inputType;
+	}
+	public double getInput() {
+		return input;
+	}
+	public void setInput(double input) {
+		this.input = input;
+	}
+	public String getOutputType() {
+		return outputType;
+	}
+	public void setOutputType(String outputType) {
+		this.outputType = outputType;
+	}
+	public double getResult() {
+		return result;
+	}
+	public void setResult(double result) {
+		this.result = result;
+	}
 
 }
--- src/main/java/com/playsafe/conversions/validators/InputValidators.java
+++ src/main/java/com/playsafe/conversions/validators/InputValidators.java
@@ -2,4 +2,15 @@ package com.playsafe.conversions.validators;
 
 public class InputValidators {
 
+	public static double stringToDouble(String input) {
+		try {
+			if (input != null || !input.isEmpty()) {
+				if (input.contains(","))
+					input = input.replace(",", ".");
+			}
+			return Double.parseDouble(input);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
 }
