2d6d36ba14bde07dcbc743aad27c857b1a3424a2
chinyanga <chinyangaf@gmail.com>
2021-01-15 07:53:04 +0200

    added a service and ktoc endpoint

--- src/main/java/com/playsafe/conversions/controllers/UnitConversionsController.java
+++ src/main/java/com/playsafe/conversions/controllers/UnitConversionsController.java
@@ -1,16 +1,39 @@
 package com.playsafe.conversions.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.assessment.PlaySafe.FormatInput;
+import com.assessment.PlaySafe.models.Conversions;
+import com.playsafe.conversions.models.UnitConversions;
+import com.playsafe.conversions.services.UnitConversionService;
+import com.playsafe.conversions.validators.InputValidators;
 
 @RestController
 @RequestMapping("/conversions")
 public class UnitConversionsController {
 	
+	@Autowired
+	public UnitConversionService conversionService;
+	
 	@GetMapping("/test")
 	public String test(){
 		return "Api works";
+	}
+	
+	@GetMapping("/ktoc/input={input}")
+	public UnitConversions kelvinToCelcius(@PathVariable String input){
+		try {
+			double kelvin = InputValidators.stringToDouble(input);
+			return conversionService.kelvinToCelcius(kelvin);
+		}
+		catch(Exception e) {
+			return new UnitConversions("Invalid Input format","miles",0.00,"kilometres",0);
+		}
+		
 	}
 
 }
--- src/main/java/com/playsafe/conversions/services/UnitConversionService.java
+++ src/main/java/com/playsafe/conversions/services/UnitConversionService.java
@@ -1,5 +1,32 @@
 package com.playsafe.conversions.services;
 
+import org.springframework.stereotype.Service;
+
+import com.playsafe.conversions.models.UnitConversions;
+
+@Service
 public class UnitConversionService {
+
+	public UnitConversions kelvinToCelcius(double kelvin) {
+
+		double celcius = (double) Math.round((kelvin - 273.15) * 100) * 100.00;
+		return new UnitConversions("Success", "kelvin", kelvin, "celcius", celcius);
+	}
+
+	public UnitConversions celciusToKelvin(double celcius) {
+		double kelvin = (double) Math.round((celcius + 273.15) * 100) / 100.00;
+		return new UnitConversions("Success", "celcius", celcius, "kelvin", kelvin);
+	}
+
+	public UnitConversions kilometresToMiles(double kilometres) {
+
+		double miles = (double) Math.round((kilometres / 1.60934) * 100) / 100.00;
+		return new UnitConversions("Success", "kilometres", kilometres, "miles", miles);
+	}
+
+	public UnitConversions milesToKilometres(double miles) {
+		double kilometres = (double) Math.round((miles * 1.60934) * 100) / 100.00;
+		return new UnitConversions("Success", "miles", miles, "kilometres", kilometres);
+	}
 
 }
